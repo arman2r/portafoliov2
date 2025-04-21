85d71c952caf6f418228fc213c48f884cfb6905c
Armando Rubio <arman.2.r@gmail.com>
2025-04-21 16:12:22 -0500

    server

--- shared-data/src/index.ts
+++ shared-data/src/index.ts
@@ -2,6 +2,7 @@ export * from './lib/shared-data';
 export * from './lib/Interfaces/experience.interface';
 export * from './lib/Interfaces/skill.interface';
 export * from './lib/environments/environment';
+export * from './lib/environments/environment.server';
 export * from './lib/Services/experience-service/experience.service';
 export * from './lib/Services/skills-service/skills.service';
 export * from './lib/Tokens/api-url.token';
