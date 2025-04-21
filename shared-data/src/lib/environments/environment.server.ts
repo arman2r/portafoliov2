d3dadc536b46a159ba850adb626d50f80e45e75b
Armando Rubio <arman.2.r@gmail.com>
2025-04-21 16:37:30 -0500

    config vercel

--- shared-data/src/lib/environments/environment.server.ts
+++
@@ -1,6 +0,0 @@
-// shared-data/src/lib/environments/environment.server.ts
-export const serverEnvironment = {
-    production: process.env['NODE_ENV'] === 'production',
-    apiUrl: process.env['NX_PUBLIC_API_URL'] || 'http://localhost:4200',
-    remoteUrlMode: process.env['REMOTE_URL_MODE'] || 'development'
-};
