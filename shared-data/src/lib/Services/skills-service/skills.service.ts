b02d2c441e22a39649b8a521b58e3f39b9f9e6c1
Armando Rubio <arman.2.r@gmail.com>
2025-04-21 10:14:20 -0500

    config env

--- shared-data/src/lib/Services/skills-service/skills.service.ts
+++ shared-data/src/lib/Services/skills-service/skills.service.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ISkill } from '../../Interfaces/skill.interface';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -11,12 +12,15 @@ export class SkillsService {
 
   private skillsUrl!: string; // Ajusta la ruta si es necesario
 
-  constructor(private http: HttpClient, @Inject(API_BASE_URL) private apiUrl: string) {   
-    console.log('API base URL:', this.apiUrl);  
+  constructor(private http: HttpClient, @Inject(API_BASE_URL) private apiUrl: string) {
+    console.log('API base URL:', this.apiUrl);
+    console.log('API base URL directly from environment:', environment.apiUrl);
+    console.log('Production mode:', environment.production);
   }
 
   getSkills(): Observable<ISkill[]> {
-    this.skillsUrl = `${this.apiUrl}/assets/data-mock/skills.json`;
+    const baseUrl = this.apiUrl || environment.apiUrl;
+    this.skillsUrl = `${baseUrl}/assets/data-mock/skills.json`;
     return this.http.get<ISkill[]>(this.skillsUrl);
   }
 }
